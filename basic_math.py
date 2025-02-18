d4260e30c3e8100f8cff9097c715ffd440b37c71
Enoch Oyeniyi <enoch.oyeniyi@stu.ocsb.ca>
2025-02-18 17:34:35 +0000

    Added basic_math.py

---
+++ basic_math.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Created By: Enoch
+# Date: Sep. 18, 2022
+# This program does basic math.
+
+
+def main():
+    print("2-1={}".format(2 - 1))
+    print("2+4={}".format((2 + 4)))
+    print("8*6={}".format(8 * 6))
+    print("4/2={}".format(4 / 2))
+    print("5/2={}".format(5 / 2))
+    print("2+5*2={}".format((2 + 5 * 2)))
+    print("2^3={}".format(2**3))  # exponent:
+    print("16**0.5={}".format(16**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
